2f3b606da8de6a20f53e1103e97baf941fb46f81
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-21 12:30:22 +0200

    Fix app.module.ts: use TokenAuthenticationPluginModule and tokenAuthenticationPluginSpecification

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -98,7 +98,7 @@ import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {environment} from "../environments/environment";
 import {registerDocumentenApiFormioUploadComponent, ZgwModule} from "@valtimo/zgw";
 
-import {SamplePluginModule, samplePluginSpecification,} from "@valtimo-plugins/token-authentication";
+import {TokenAuthenticationPluginModule, tokenAuthenticationPluginSpecification,} from "@valtimo-plugins/token-authentication";
 
 export function tabsFactory() {
   return new Map<string, object>([
@@ -158,7 +158,7 @@ export function tabsFactory() {
     ProcessModule,
     ReactiveFormsModule,
     ResourceModule,
-    SamplePluginModule,
+    TokenAuthenticationPluginModule,
     SecurityModule,
     SseModule,
     SwaggerModule,
@@ -190,7 +190,7 @@ export function tabsFactory() {
         objecttypenApiPluginSpecification,
         openNotificatiesPluginSpecification,
         openZaakPluginSpecification,
-        samplePluginSpecification,
+        tokenAuthenticationPluginSpecification,
         zakenApiPluginSpecification,
       ],
     },
